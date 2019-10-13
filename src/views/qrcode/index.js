f8df49fd1dd0ef1dd20e1a6de0cec2515af0f317
Bios Sun <biossun@gmail.com>
2019-10-14 00:49:38 +0800

    qrcode - 修复无法输入空白字符的问题；

--- src/views/qrcode/index.js
+++ src/views/qrcode/index.js
@@ -6,11 +6,6 @@ import styles from './index.module.scss'
 export default function QRCodeView() {
     const [text, setText] = useState('')
 
-    function handleTextareaChange(e) {
-        const text = (e.target.value || '').trim();
-        setText(text);
-    }
-
     return (
         <>
             <h1>QRCode</h1>
@@ -19,10 +14,10 @@ export default function QRCodeView() {
                 <textarea
                     className={styles.textarea}
                     value={text}
-                    onChange={handleTextareaChange}
+                    onChange={e => setText(e.target.value || '')}
                     placeholder="请输入文本"
                 />
-                {text ? <QRCode className={styles.qrcode} value={text} size={256} /> : null}
+                {text ? <QRCode className={styles.qrcode} value={text.trim()} size={256} /> : null}
             </div>
         </>
     )
