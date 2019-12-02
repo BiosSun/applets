f95bb43bfd16294f5b412dc872f9fb5bb9086885
Bios Sun <biossun@gmail.com>
2019-12-02 23:57:36 +0800

    qrcode - 修复文本过长时页面报错的问题；

--- src/views/qrcode/index.js
+++ src/views/qrcode/index.js
@@ -3,22 +3,46 @@ import QRCode from 'qrcode.react'
 
 import styles from './index.module.scss'
 
+const MAX_TEXT_LENGTH = 1024
+const LONG_TEXT = 256
+
 export default function QRCodeView() {
     const [text, setText] = useState('')
+
+    const payload = text.trim()
+    const isShort = payload.length < LONG_TEXT
+    const size = isShort ? 256 : 512
 
     return (
         <>
             <h1>QRCode</h1>
             <p>该工具用于生成一个二维码</p>
             <div className={styles.container}>
-                <textarea
-                    className={styles.textarea}
-                    value={text}
-                    onChange={e => setText(e.target.value || '')}
-                    placeholder="请输入文本"
-                />
-                {text ? <QRCode className={styles.qrcode} value={text.trim()} size={256} /> : null}
+                <Input value={text} onChange={setText} height={size} />
+                <Display value={payload} size={size} />
             </div>
         </>
     )
+}
+
+function Input({ value, onChange, height }) {
+    return (
+        <textarea
+            className={styles.textarea}
+            value={value}
+            onChange={e => onChange(e.target.value)}
+            placeholder="请输入文本"
+            style={{ height }}
+        />
+    )
+}
+
+function Display({ value, size }) {
+    if (!value) {
+        return null
+    } else if (value.length > MAX_TEXT_LENGTH) {
+        return <span className={styles.dangerMessage}>不可超过 1024 个字符</span>
+    } else {
+        return <QRCode className={styles.qrcode} value={value} size={size} />
+    }
 }
