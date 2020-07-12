3bf144146d3a54a698acc2339c74889215dbe615
Bios Sun <biossun@gmail.com>
2020-07-12 16:42:18 +0800

    JSON 支持展开/折叠；

--- src/views/json/index.js
+++ src/views/json/index.js
@@ -1,10 +1,11 @@
 import React, { useMemo } from 'react'
 import clsx from 'clsx'
-import { VLinear, HLinear, Divider } from '@biossun/nami'
+import { VLinear, HLinear, Divider, Space, Button } from '@biossun/nami'
 import _ from 'lodash'
+import { useToggle } from 'react-use'
+import useLocalState from 'utils/use-local-state'
 
 import styles from './index.module.scss'
-import useLocalState from 'utils/use-local-state'
 
 const DEFAULT_TEXT =
     '{"ID":null,"name":"Doe","first-name":"John","age":25,"hobbies":["reading","cinema",{"sports":["volley-ball","badminton"]}],"address":{}}'
@@ -13,11 +14,20 @@ export default function JSONView() {
     const [text, setText] = useLocalState('JSON/text', DEFAULT_TEXT)
 
     return (
-        <VLinear className={styles.container} padding="large" spacing>
-            <h1>JSON</h1>
-            <p>解析 JSON 字符串</p>
+        <VLinear className={styles.container}>
+            <VLinear padding="large" spacing>
+                <h1>JSON</h1>
+                <p>解析 JSON 字符串</p>
 
-            <HLinear $flex spacing="large">
+                <HLinear spacing align="center">
+                    <Space $flex />
+
+                </HLinear>
+            </VLinear>
+
+            <Divider />
+
+            <HLinear className={styles.content} $flex>
                 <Input $flex $col={10} value={text} onChange={setText} />
 
                 <Divider />
@@ -98,32 +108,58 @@ function PropertyValue({ value }) {
     } else if (_.isString(value)) {
         content = <span className={styles.string}>"{value}"</span>
     } else if (_.isArray(value)) {
-        content = (
-            <span className={styles.array}>
-                <span className={styles.bracket}>[</span>
-                <div className={styles.items}>
-                    {value.map((item, index) => (
-                        <PropertyItem key={index} value={item} />
-                    ))}
-                </div>
-                <span className={styles.bracket}>]</span>
-            </span>
-        )
+        content = <ArrayPropertyValue value={value} />
     } else if (_.isObject(value)) {
-        content = (
-            <span className={styles.object}>
-                <span className={styles.bracket}>{'{'}</span>
-                <div className={styles.items}>
-                    {Object.entries(value).map(([key, value]) => (
-                        <PropertyItem key={key} name={key} value={value} />
-                    ))}
-                </div>
-                <span className={styles.bracket}>{'}'}</span>
-            </span>
-        )
+        content = <ObjectPropertyValue value={value} />
     } else {
         content = null
     }
 
     return content
+}
+
+function ArrayPropertyValue({ value }) {
+    const [on, toggle] = useToggle(true)
+
+    return (
+        <span className={clsx(styles.array, { [styles.expand]: on })}>
+            <span className={clsx(styles.bracket, styles.leftBracket)} onClick={toggle}>
+                [
+            </span>
+
+            <div className={styles.length}>{value.length}</div>
+
+            <div className={styles.items}>
+                {value.map((item, index) => (
+                    <PropertyItem key={index} value={item} />
+                ))}
+            </div>
+
+            <span className={clsx(styles.bracket, styles.rightBracket)}>]</span>
+        </span>
+    )
+}
+
+function ObjectPropertyValue({ value }) {
+    const [on, toggle] = useToggle(true)
+
+    const entries = Object.entries(value)
+
+    return (
+        <span className={clsx(styles.object, { [styles.expand]: on })}>
+            <span className={clsx(styles.bracket, styles.leftBracket)} onClick={toggle}>
+                {'{'}
+            </span>
+
+            <div className={styles.length}>{entries.length}</div>
+
+            <div className={styles.items}>
+                {entries.map(([key, value]) => (
+                    <PropertyItem key={key} name={key} value={value} />
+                ))}
+            </div>
+
+            <span className={clsx(styles.bracket, styles.rightBracket)}>{'}'}</span>
+        </span>
+    )
 }
