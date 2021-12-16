b957ae59277b744b02af6cb61aa20493f1214272
Bios Sun <biossun@gmail.com>
2021-12-16 20:02:21 +0800

    JSON - 支持格式化时间戳

--- src/views/json/index.js
+++ src/views/json/index.js
@@ -7,6 +7,7 @@ import { Divider } from '@nami-ui/divider'
 import useLocalState from 'utils/use-local-state.ts'
 import Panel from 'components/panel'
 import FileSize from 'components/file-size'
+import dayjs from 'dayjs'
 
 import styles from './index.module.scss'
 
@@ -37,7 +38,9 @@ export default function JSONView() {
     const [text, setText] = useLocalState('JSON/text', DEFAULT_TEXT)
     const [decode, setDecode] = useLocalState('JSON/decode', false)
     const [deep, setDeep] = useLocalState('JSON/deep', false)
+    const [timestamp, setTimestamp] = useLocalState('JSON/timestamp', false)
     const [arrayIndex, setArrayIndex] = useLocalState('JSON/arrayIndex', false)
+    const [sortProps, setSortProps] = useLocalState('JSON/sortProps', false)
 
     function formatText() {
         const [json, error] = parseJSON(text)
@@ -83,6 +86,19 @@ export default function JSONView() {
                         component="label"
                         spacing="small"
                         align="center"
+                        title="对于数值类型的值，若其值大于等于 1 年前的 Unix 时间戳，且小于等于 1 年后的 Unix 时间戳，则将其当作 Unix 时间戳进行解析"
+                    >
+                        <input
+                            type="checkbox"
+                            checked={!!timestamp}
+                            onChange={(event) => setTimestamp(event.target.checked ? 'ms' : false)}
+                        />
+                        解码时间戳（毫秒）
+                    </HStack>
+                    <HStack
+                        component="label"
+                        spacing="small"
+                        align="center"
                         title="对于 JSON 格式的字符串类型的值，直接解析并渲染其 JSON 数据"
                     >
                         <input
@@ -105,6 +121,19 @@ export default function JSONView() {
                         />
                         数组索引
                     </HStack>
+                    <HStack
+                        component="label"
+                        spacing="small"
+                        align="center"
+                        title="按属性值进行排序"
+                    >
+                        <input
+                            type="checkbox"
+                            checked={sortProps}
+                            onChange={(event) => setSortProps(event.target.checked)}
+                        />
+                        排序属性
+                    </HStack>
                     <button onClick={formatText}>格式化源码</button>
                     <button onClick={compressText}>压缩源码源码</button>
                 </HStack>
@@ -118,7 +147,9 @@ export default function JSONView() {
                     $col={14}
                     text={text}
                     decode={decode}
+                    timestamp={timestamp}
                     deep={deep}
+                    sortProps={sortProps}
                     arrayIndex={arrayIndex}
                 />
             </HStack>
@@ -180,6 +211,30 @@ function parseJSON(text) {
     }
 }
 
+const now = dayjs()
+const VALID_TIMESTAMP_NUMBER_RANGE = [
+    now.subtract(10, 'years').valueOf(),
+    now.add(10, 'years').valueOf(),
+]
+
+function parseNumber(number, timestamp) {
+    let value = number
+    let type = 'number'
+
+    console.info(number, timestamp, VALID_TIMESTAMP_NUMBER_RANGE)
+
+    if (
+        timestamp &&
+        value >= VALID_TIMESTAMP_NUMBER_RANGE[0] &&
+        value <= VALID_TIMESTAMP_NUMBER_RANGE[1]
+    ) {
+        value = dayjs(value)
+        type = 'time'
+    }
+
+    return [value, type]
+}
+
 function parseString(string, decode, deep) {
     let value = string
     let type = 'text'
@@ -238,9 +293,39 @@ function tryParseURL(text) {
     }
 }
 
-function Display({ text, decode, deep, arrayIndex, ...otherProps }) {
+function getType(value) {
+    return _.isNull(value)
+        ? 'null'
+        : _.isBoolean(value)
+        ? 'boolean'
+        : _.isNumber(value)
+        ? 'number'
+        : _.isString(value)
+        ? 'string'
+        : _.isArray(value)
+        ? 'array'
+        : _.isObject(value)
+        ? 'object'
+        : 'unknown'
+}
+
+const TYPE_SORTS = {
+    unknown: 0,
+    null: 1,
+    boolean: 2,
+    number: 3,
+    string: 4,
+    array: 5,
+    object: 6,
+}
+
+function Display({ text, decode, timestamp, deep, sortProps, arrayIndex, ...otherProps }) {
     const [value, error] = useMemo(() => parseJSON(text), [text])
-    const context = useMemo(() => ({ decode, deep, arrayIndex }), [decode, deep, arrayIndex])
+
+    const context = useMemo(
+        () => ({ decode, deep, timestamp, sortProps, arrayIndex }),
+        [decode, deep, timestamp, sortProps, arrayIndex]
+    )
 
     return (
         <DisplayContext.Provider value={context}>
@@ -274,7 +359,7 @@ function PropertyValue({ value }) {
     } else if (_.isBoolean(value)) {
         content = <span className={styles.boolean}>{value ? 'true' : 'false'}</span>
     } else if (_.isNumber(value)) {
-        content = <span className={styles.number}>{value}</span>
+        content = <NumberPropertyValue value={value} />
     } else if (_.isString(value)) {
         content = <StringPropertyValue value={value} />
     } else if (_.isArray(value)) {
@@ -286,6 +371,25 @@ function PropertyValue({ value }) {
     }
 
     return content
+}
+
+function NumberPropertyValue({ value: num }) {
+    const { timestamp } = useContext(DisplayContext)
+
+    const [value, type] = useMemo(() => parseNumber(num, timestamp), [num, timestamp])
+
+    switch (type) {
+        case 'number':
+            return <span className={styles.number}>{value}</span>
+        case 'time':
+            return (
+                <span className={styles.time} title={num}>
+                    {value.format('YYYY-MM-DD HH:mm:ss.SSS')}
+                </span>
+            )
+        default:
+            throw new Error('无效的数值类型')
+    }
 }
 
 function StringPropertyValue({ value: str }) {
@@ -343,9 +447,34 @@ function ArrayPropertyValue({ value }) {
 }
 
 function ObjectPropertyValue({ value }) {
+    const { sortProps } = useContext(DisplayContext)
     const [on, toggle] = useToggle(true)
 
-    const entries = Object.entries(value)
+    const entries = useMemo(() => {
+        return Object.entries(value)
+    }, [value])
+
+    const sortedEntries = useMemo(() => {
+        if (!sortProps) {
+            return entries
+        }
+
+        return [...entries].sort(([key1, value1], [key2, value2]) => {
+            const type1 = getType(value1)
+            const type2 = getType(value2)
+
+            const typeIndex1 = TYPE_SORTS[type1]
+            const typeIndex2 = TYPE_SORTS[type2]
+
+            if (typeIndex1 !== typeIndex2) {
+                return typeIndex1 - typeIndex2
+            }
+
+            return value1 < value2 ? -1 : value1 > value2 ? 1 : 0
+        })
+    }, [entries, sortProps])
+
+    const finalEntries = sortedEntries
 
     return (
         <span className={clsx(styles.object, { [styles.fold]: !on })}>
@@ -353,15 +482,15 @@ function ObjectPropertyValue({ value }) {
                 {'{'}
             </span>
 
-            <div className={styles.length}>{entries.length}</div>
+            <div className={styles.length}>{finalEntries.length}</div>
 
             <div className={styles.items}>
-                {entries.map(([key, value], index) => (
+                {finalEntries.map(([key, value], index) => (
                     <PropertyItem
                         key={key}
                         name={key}
                         value={value}
-                        last={index === entries.length - 1}
+                        last={index === finalEntries.length - 1}
                     />
                 ))}
             </div>
