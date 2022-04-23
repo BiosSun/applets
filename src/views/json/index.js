47b625012d2cfd27fe60aa482dd153889f5d0a83
Bios Sun <biossun@gmail.com>
2022-04-23 23:54:10 +0800

    JSON - 支持注释

--- src/views/json/index.js
+++ src/views/json/index.js
@@ -1,13 +1,17 @@
-import { useMemo, createContext, useContext, useState, useEffect } from 'react'
+import { useMemo, createContext, useContext, useState, useEffect, useCallback } from 'react'
 import _ from 'lodash'
 import clsx from 'clsx'
+import dayjs from 'dayjs'
 import { useToggle } from 'react-use'
+
 import { VStack, HStack } from '@nami-ui/stack'
 import { Divider } from '@nami-ui/divider'
-import useLocalState from 'utils/use-local-state.ts'
 import Panel from 'components/panel'
 import FileSize from 'components/file-size'
-import dayjs from 'dayjs'
+import Toggle from 'components/toggle'
+
+import useLocalState from 'utils/use-local-state.ts'
+import { adaptors } from './adaptors'
 
 import styles from './index.module.scss'
 
@@ -34,6 +38,8 @@ const DEFAULT_TEXT = `{
     "address": {}
 }`
 
+const JSONContext = createContext()
+
 export default function JSONView() {
     const [text, setText] = useLocalState('JSON/text', DEFAULT_TEXT)
     const [decode, setDecode] = useLocalState('JSON/decode', false)
@@ -41,104 +47,135 @@ export default function JSONView() {
     const [timestamp, setTimestamp] = useLocalState('JSON/timestamp', false)
     const [arrayIndex, setArrayIndex] = useLocalState('JSON/arrayIndex', false)
     const [sortProps, setSortProps] = useLocalState('JSON/sortProps', false)
+    const [comment, setComment] = useLocalState('JSON/comment', false)
 
-    function formatText() {
+    const JSON = comment ? adaptors.comment : adaptors.native
+
+    const parseJSON = useCallback(
+        (text) => {
+            const trimmedText = text?.trim() ?? ''
+
+            if (!trimmedText) {
+                return [undefined, undefined]
+            }
+
+            if (!JSON.initialValid(trimmedText)) {
+                const error = new Error('无效的 JSON 源码')
+                if (JSON !== adaptors.comment && adaptors.comment.initialValid(trimmedText)) {
+                    error.message += '，若您的 JSON 中包含注释，请勾选「支持注释」'
+                }
+
+                return [undefined, error]
+            }
+
+            try {
+                const value = JSON.parse(trimmedText)
+                return [value]
+            } catch (error) {
+                return [undefined, error]
+            }
+        },
+        [JSON]
+    )
+
+    function formatJSON() {
         const [json, error] = parseJSON(text)
 
         if (error) {
             alert(error.message)
         } else {
-            setText(JSON.stringify(json, null, 4))
+            setText(JSON.stringify(json, 4))
         }
     }
 
-    function compressText() {
+    function compressJSON() {
         const [json, error] = parseJSON(text)
 
         if (error) {
             alert(error.message)
         } else {
-            setText(JSON.stringify(json, null, 0))
+            setText(JSON.stringify(json, 0))
         }
     }
 
     return (
-        <VStack className={styles.container}>
-            <HStack spacing padding align="center">
-                <HStack
-                    component="label"
-                    spacing="small"
-                    align="center"
-                    title="对于字符串类型的值，尝试使用 decodeURIComponent 进行解码"
-                >
-                    <input
-                        type="checkbox"
-                        checked={decode}
-                        onChange={(event) => setDecode(event.target.checked)}
+        <JSONContext.Provider value={JSON}>
+            <VStack className={styles.container}>
+                <HStack spacing padding align="center">
+                    <HStack
+                        component="label"
+                        spacing="small"
+                        align="center"
+                        title="对于字符串类型的值，尝试使用 decodeURIComponent 进行解码"
+                    >
+                        <input type="checkbox" checked={decode} onChange={(event) => setDecode(event.target.checked)} />
+                        解码字符串
+                    </HStack>
+                    <HStack
+                        component="label"
+                        spacing="small"
+                        align="center"
+                        title="对于数值类型的值，若其值大于等于 1 年前的 Unix 时间戳，且小于等于 1 年后的 Unix 时间戳，则将其当作 Unix 时间戳进行解析"
+                    >
+                        <input
+                            type="checkbox"
+                            checked={!!timestamp}
+                            onChange={(event) => setTimestamp(event.target.checked ? 'ms' : false)}
+                        />
+                        解码时间戳（毫秒）
+                    </HStack>
+                    <HStack
+                        component="label"
+                        spacing="small"
+                        align="center"
+                        title="对于 JSON 格式的字符串类型的值，直接解析并渲染其 JSON 数据"
+                    >
+                        <input type="checkbox" checked={deep} onChange={(event) => setDeep(event.target.checked)} />
+                        深层解析
+                    </HStack>
+                    <HStack component="label" spacing="small" align="center" title="显示数组元素的索引">
+                        <input
+                            type="checkbox"
+                            checked={arrayIndex}
+                            onChange={(event) => setArrayIndex(event.target.checked)}
+                        />
+                        数组索引
+                    </HStack>
+                    <HStack component="label" spacing="small" align="center" title="按属性值进行排序">
+                        <input
+                            type="checkbox"
+                            checked={sortProps}
+                            onChange={(event) => setSortProps(event.target.checked)}
+                        />
+                        排序属性
+                    </HStack>
+                    <Toggle
+                        label="支持注释"
+                        title="允许源码中包含注释"
+                        value={comment}
+                        onChange={(checked) => setComment(checked)}
                     />
-                    解码字符串
+                    <button onClick={formatJSON}>格式化源码</button>
+                    <button onClick={compressJSON}>压缩源码源码</button>
                 </HStack>
-                <HStack
-                    component="label"
-                    spacing="small"
-                    align="center"
-                    title="对于数值类型的值，若其值大于等于 1 年前的 Unix 时间戳，且小于等于 1 年后的 Unix 时间戳，则将其当作 Unix 时间戳进行解析"
-                >
-                    <input
-                        type="checkbox"
-                        checked={!!timestamp}
-                        onChange={(event) => setTimestamp(event.target.checked ? 'ms' : false)}
-                    />
-                    解码时间戳（毫秒）
-                </HStack>
-                <HStack
-                    component="label"
-                    spacing="small"
-                    align="center"
-                    title="对于 JSON 格式的字符串类型的值，直接解析并渲染其 JSON 数据"
-                >
-                    <input
-                        type="checkbox"
-                        checked={deep}
-                        onChange={(event) => setDeep(event.target.checked)}
-                    />
-                    深层解析
-                </HStack>
-                <HStack component="label" spacing="small" align="center" title="显示数组元素的索引">
-                    <input
-                        type="checkbox"
-                        checked={arrayIndex}
-                        onChange={(event) => setArrayIndex(event.target.checked)}
-                    />
-                    数组索引
-                </HStack>
-                <HStack component="label" spacing="small" align="center" title="按属性值进行排序">
-                    <input
-                        type="checkbox"
-                        checked={sortProps}
-                        onChange={(event) => setSortProps(event.target.checked)}
-                    />
-                    排序属性
-                </HStack>
-                <button onClick={formatText}>格式化源码</button>
-                <button onClick={compressText}>压缩源码源码</button>
-            </HStack>
 
-            <HStack $flex className={styles.panels}>
-                <Input $flex $col={10} value={text} onChange={setText} />
-                <Divider />
-                <Display
-                    $flex
-                    $col={14}
-                    text={text}
-                    decode={decode}
-                    timestamp={timestamp}
-                    deep={deep}
-                    sortProps={sortProps}
-                    arrayIndex={arrayIndex}
-                />
-            </HStack>
-        </VStack>
+                <HStack $flex className={styles.panels}>
+                    <Input $flex $col={10} value={text} onChange={setText} />
+                    <Divider />
+                    <Display
+                        $flex
+                        $col={14}
+                        text={text}
+                        decode={decode}
+                        timestamp={timestamp}
+                        deep={deep}
+                        sortProps={sortProps}
+                        arrayIndex={arrayIndex}
+                        parseJSON={parseJSON}
+                    />
+                </HStack>
+            </VStack>
+        </JSONContext.Provider>
     )
 }
 
@@ -180,37 +217,14 @@ function SizesInfo({ sourceCode }) {
 
 const DisplayContext = createContext({ decode: false })
 
-const r_valid_json = /^[{["\-\ntfn]/
-
-function parseJSON(text) {
-    const trimedText = _.trim(text)
-
-    if (!trimedText || !r_valid_json.test(trimedText)) {
-        return [undefined, undefined]
-    }
-
-    try {
-        return [JSON.parse(trimedText)]
-    } catch (error) {
-        return [undefined, error]
-    }
-}
-
 const now = dayjs()
-const VALID_TIMESTAMP_NUMBER_RANGE = [
-    now.subtract(10, 'years').valueOf(),
-    now.add(10, 'years').valueOf(),
-]
+const VALID_TIMESTAMP_NUMBER_RANGE = [now.subtract(10, 'years').valueOf(), now.add(10, 'years').valueOf()]
 
 function parseNumber(number, timestamp) {
     let value = number
     let type = 'number'
 
-    if (
-        timestamp &&
-        value >= VALID_TIMESTAMP_NUMBER_RANGE[0] &&
-        value <= VALID_TIMESTAMP_NUMBER_RANGE[1]
-    ) {
+    if (timestamp && value >= VALID_TIMESTAMP_NUMBER_RANGE[0] && value <= VALID_TIMESTAMP_NUMBER_RANGE[1]) {
         value = dayjs(value)
         type = 'time'
     }
@@ -218,7 +232,7 @@ function parseNumber(number, timestamp) {
     return [value, type]
 }
 
-function parseString(string, decode, deep) {
+function parseString(string, decode, deep, parseJSON) {
     let value = string
     let type = 'text'
 
@@ -227,7 +241,7 @@ function parseString(string, decode, deep) {
     }
 
     if (deep) {
-        ;[value, type] = tryParseJSON(value)
+        ;[value, type] = tryParseJSON(value, parseJSON)
     }
 
     if (type === 'text') {
@@ -237,8 +251,8 @@ function parseString(string, decode, deep) {
     return [value, type]
 }
 
-function tryParseJSON(text) {
-    const [value, error] = parseJSON(text)
+function tryParseJSON(text, parser) {
+    const [value, error] = parser(text)
 
     if (error || value === undefined) {
         return [text, 'text']
@@ -302,12 +316,12 @@ const TYPE_SORTS = {
     object: 6,
 }
 
-function Display({ text, decode, timestamp, deep, sortProps, arrayIndex, ...otherProps }) {
-    const [value, error] = useMemo(() => parseJSON(text), [text])
+function Display({ text, decode, timestamp, deep, sortProps, arrayIndex, parseJSON, ...otherProps }) {
+    const [value, error] = useMemo(() => parseJSON(text), [parseJSON, text])
 
     const context = useMemo(
-        () => ({ decode, deep, timestamp, sortProps, arrayIndex }),
-        [decode, deep, timestamp, sortProps, arrayIndex]
+        () => ({ decode, deep, timestamp, sortProps, arrayIndex, parseJSON }),
+        [decode, deep, timestamp, sortProps, arrayIndex, parseJSON]
     )
 
     return (
@@ -376,21 +390,16 @@ function NumberPropertyValue({ value: num }) {
 }
 
 function StringPropertyValue({ value: str }) {
-    const { decode, deep } = useContext(DisplayContext)
+    const { decode, deep, parseJSON } = useContext(DisplayContext)
 
-    const [value, type] = useMemo(() => parseString(str, decode, deep), [str, decode, deep])
+    const [value, type] = useMemo(() => parseString(str, decode, deep, parseJSON), [str, decode, deep, parseJSON])
 
     switch (type) {
         case 'text':
             return <span className={styles.string}>"{value}"</span>
         case 'url':
             return (
-                <a
-                    className={styles.string}
-                    href={value.toString()}
-                    target="_blank"
-                    rel="noreferrer"
-                >
+                <a className={styles.string} href={value.toString()} target="_blank" rel="noreferrer">
                     {value.toString()}
                 </a>
             )
@@ -469,12 +478,7 @@ function ObjectPropertyValue({ value }) {
 
             <div className={styles.items}>
                 {finalEntries.map(([key, value], index) => (
-                    <PropertyItem
-                        key={key}
-                        name={key}
-                        value={value}
-                        last={index === finalEntries.length - 1}
-                    />
+                    <PropertyItem key={key} name={key} value={value} last={index === finalEntries.length - 1} />
                 ))}
             </div>
 
